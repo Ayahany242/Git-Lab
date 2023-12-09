6f18cc4b431fe7763695e63b3e118c46eabf1bcb
Aya <aayahanyy@gmail.com>
2023-12-09 14:14:45 +0200

    add files .java

---
+++ HelloITI.java
@@ -0,0 +1,11 @@
+public class HelloITI{
+	
+	
+	public void printHelloITI(){
+		System.out.println("HelloWorld");
+	}
+	public void printHelloGitto(){
+		System.out.println("printHelloGitto");
+		
+	}
+}
